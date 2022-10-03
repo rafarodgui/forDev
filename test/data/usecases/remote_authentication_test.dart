38978f32b62a11aa4574939398b7fd5c200fb64d
rafaelRodruiguesGuimaraes <rafael.guimaraes@pagtel.com.br>
2022-10-03 16:31:25 -0300

    ensure remote authentication throws unexpected error on 404

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -47,15 +47,32 @@ void main() {
   );
 
   test(
-    'Should throw UnexpectedError if HttpCliente returns 400',
+    'Should throw UnexpectedError if HttpCliente returns status 400',
     () async {
       when(
         httpClient.request(
           url: anyNamed('url'),
-          method: anyNamed(' method'),
+          method: anyNamed('method'),
           body: anyNamed('body'),
         ),
       ).thenThrow(HttpError.badRequest);
+
+      final future = sut.auth(params);
+
+      expect(future, throwsA(DomainError.unexpected));
+    },
+  );
+
+  test(
+    'Should throw UnexpectedError if HttpCliente returns status 404',
+    () async {
+      when(
+        httpClient.request(
+          url: anyNamed('url'),
+          method: anyNamed('method'),
+          body: anyNamed('body'),
+        ),
+      ).thenThrow(HttpError.notFound);
 
       final future = sut.auth(params);
 
