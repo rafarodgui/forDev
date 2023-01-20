c05455cc0901be79681a1141f3076ab996e4f9ea
rafaelRodruiguesGuimaraes <rafael.guimaraes@pagtel.com.br>
2023-01-20 17:51:31 -0300

    ensure httpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -12,12 +14,14 @@ class HttpAdapter {
   Future<void> request({
     @required String url,
     @required String method,
+    Map body,
   }) async {
     final headers = {
       'content-type': 'aplication/json',
       'accept': 'aplication/json',
     };
-    await client.post(Uri.parse(url), headers: headers);
+    final verifyBodyExists = body != null ? jsonEncode(body) : null;
+    await client.post(Uri.parse(url), headers: headers, body: verifyBodyExists);
   }
 }
 
@@ -37,24 +41,30 @@ void main() {
   group('post', () {
     test('shoul call post with correct values', () async {
       //action
-      await sut.request(url: url, method: 'post');
+      await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
-      verify(client.post(
-        Uri.parse(url),
-      ));
+      verify(
+        client.post(
+          Uri.parse(url),
+          headers: {
+            'content-type': 'aplication/json',
+            'accept': 'aplication/json',
+          },
+          body: '{"any_key":"any_value"}',
+        ),
+      );
     });
 
-    test('shoul call post with correct headers', () async {
+    test('shoul call post without body', () async {
       //action
       await sut.request(url: url, method: 'post');
 
-      verify(client.post(
-        Uri.parse(url),
-        headers: {
-          'content-type': 'aplication/json',
-          'accept': 'aplication/json',
-        },
-      ));
+      verify(
+        client.post(
+          any,
+          headers: anyNamed('headers'),
+        ),
+      );
     });
   });
 }
